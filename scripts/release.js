cda4f654bc991497630a33ee951576d670a03d21
nick cherny <nickcherny@nicks-MacBook-Pro.local>
2018-11-13 22:31:20 +0300

    (feat)
    BUILDER:
    
    Configurate release builder

--- scripts/release.js
+++ scripts/release.js
@@ -1,48 +1,38 @@
-const { resolve, relative } = require('path');
-const { writeFileSync } = require('fs');
-const {
-    rm, exec, exit, touch, cp
-} = require('shelljs');
+const {resolve, relative} = require('path');
+const {writeFileSync} = require('fs');
+const {rm, exec, exit, touch, cp} = require('shelljs');
 const glob = require('glob');
-const { SRC, DIST } = require('./utils/getPackagesName');
-const {
-    showAllPackages,
-    getPackageName,
-    getPackageVersion
-} = require('./utils/outerInterface');
+const {showAllPackages, getPackageName, getPackageVersion} = require('./utils/outerInterface');
+const { path: { dist, source }, formats } = require('./settings.json');
 
 
 const isPublish = process.argv.includes('--publish');
-const DIST_PACKAGE_PATH = resolve(process.cwd(), './dist');
-const SRC_PACKAGE_PATH = resolve(process.cwd(), './lib');
+
+const ROOT_PATH = process.cwd();
+const DIST_PACKAGE_PATH = resolve(ROOT_PATH, dist);
+const SRC_PACKAGE_PATH = resolve(ROOT_PATH, source);
 const MODULES_BIN_PATH = './node_modules/.bin';
+const ROLLUP_BIN_PATH = resolve(MODULES_BIN_PATH, 'rollup');
+
+function runRollup(formatType) {
+  return `BABEL_ENV=rollup ${ROLLUP_BIN_PATH} --config scripts/rollup.config.js --environment BUILD:${formatType}`;
+}
 
 rm('-rf', DIST_PACKAGE_PATH);
 console.log(`distination dir clear!`);
 
-const sourceFiles = glob.sync(
-    resolve(process.cwd(), './lib/', './**/*.js'),
-    { ignore: `${SRC_PACKAGE_PATH}/node_modules/**/*.js` }
-).map(to => relative(SRC, to));
+const sourceFiles = glob.sync(resolve(ROOT_PATH, './lib/', './**/*.js'), { ignore: `${SRC_PACKAGE_PATH}/node_modules/**/*.js` }).map(to => relative(SRC_PACKAGE_PATH, to));
 
-exec(
-    `cd ${SRC_PACKAGE_PATH} && BABEL_ENV=cjs ${resolve(process.cwd(), MODULES_BIN_PATH)}/babel ${sourceFiles.join(' ')} --out-dir ${resolve(DIST)}`
-);
+exec(`cd ${SRC_PACKAGE_PATH} && BABEL_ENV=cjs ${resolve(ROOT_PATH, MODULES_BIN_PATH)}/babel ${sourceFiles.join(' ')} --out-dir ${resolve(DIST_PACKAGE_PATH)}`);
 
-const runRollup = build => (
-    `BABEL_ENV=rollup ${resolve(MODULES_BIN_PATH, 'rollup')} --config scripts/rollup.config.js --environment BUILD:${build},PACKAGE_NAME:redux-worker`
-);
-
-if (exec([runRollup('es'), runRollup('cjs'), runRollup('umd'), runRollup('min')].join(' && ')).code !== 0) {
-    exit(1);
+if (exec(formats.map(runRollup).join(' && ')).code !== 0) {
+  exit(1);
 }
 
 touch(`${DIST_PACKAGE_PATH}/README.md`);
-exec(`${resolve(MODULES_BIN_PATH, 'documentation')} build ${SRC_PACKAGE_PATH}/index.js -f md -o ${DIST_PACKAGE_PATH}/README.md`);
+// exec(`${resolve(MODULES_BIN_PATH, 'documentation')} build ${SRC_PACKAGE_PATH}/index.js -f md -o ${DIST_PACKAGE_PATH}/README.md`);
 
-if (isPublish) {
-    exec(`npm publish ${resolve(SRC, buildPackage)}`);
-    console.log('published');
-}
-
-cp(SRC_PACKAGE_CONFIG_PATH, DIST_PACKAGE_PATH);
+// if (isPublish) {
+//   exec(`npm publish ${resolve(SRC, buildPackage)}`);
+//   console.log('published');
+// }
--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,63 +1,75 @@
-const path = require('path');
+const { resolve } = require('path');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
 const replace = require('rollup-plugin-replace');
 const commonjs = require('rollup-plugin-commonjs');
 const flow = require('rollup-plugin-flow');
-const { uglify } = require('rollup-plugin-uglify');
-const { pascalCase } = require('change-case');
+const {uglify} = require('rollup-plugin-uglify');
+const {pascalCase} = require('change-case');
+
+const { path: { dist } } = require('./settings.json');
 
 
-const packageName = process.env.PACKAGE_NAME;
+const ROOT_PATH = process.cwd();
 const build = process.env.BUILD;
-const ROOT = process.cwd();
 
-const config = {
-    entry: `${ROOT}/lib/index`,
-    external: ['react'],
-    moduleName: 'redux-worker',
-    plugins: [
-        babel({ exclude: '**/node_modules/**' }),
-    ],
+/**
+ * @param {String} formatType
+ * @param {String} ?filename
+ * @return {String}
+ */
+function combineDistFilePath(formatType, filename = 'index.js') {
+  return resolve(ROOT_PATH, dist, formatType, filename);
+}
+
+const baseConfig = {
+  entry: `${ROOT_PATH}/lib/index`,
+  external: [],
+  moduleName: 'redux-worker',
+  plugins: [babel({
+    exclude: '**/node_modules/**'
+  })],
 };
 
 if (build === 'es' || build === 'cjs') {
-    config.external.push(
-        'fbjs/lib/shallowEqual',
-        'hoist-non-react-statics',
-        'change-emitter',
-        'symbol-observable',
-        'react-relay',
-        'recompose'
-    );
-    config.dest = path.resolve(ROOT, `./dist/redux-worker/${build}/index.js`);
-    config.format = build;
+  baseConfig.external.push(
+    'fbjs/lib/shallowEqual',
+    'hoist-non-react-statics',
+    'change-emitter',
+    'symbol-observable',
+    'react-relay',
+  );
+  baseConfig.dest = combineDistFilePath(build);
+  baseConfig.format = build;
 }
 
 if (build === 'umd') {
-    config.dest = path.resolve(ROOT, `./dist/redux-worker/build/index.js`);
-    config.format = 'umd';
-    config.plugins.push(
-        nodeResolve({
-            jsnext: true,
-        }),
-        commonjs(),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('development'),
-        })
-    );
+  baseConfig.dest = combineDistFilePath(build);
+  baseConfig.format = build;
+  baseConfig.plugins.push(
+    nodeResolve({
+      jsnext: true,
+    }),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    })
+  );
 }
 
 if (build === 'min') {
-    config.dest = path.resolve(ROOT, `./dist/redux-worker/build/index.min.js`);
-    config.format = 'umd';
-    config.plugins.push(
-        nodeResolve({ jsnext: true }),
-        commonjs(),
-        flow(),
-        replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-        uglify()
-    );
+  baseConfig.dest = combineDistFilePath('./')
+  baseConfig.format = 'umd';
+  baseConfig.plugins.push(
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs(),
+    flow(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+  );
 }
 
-export default config;
+export default baseConfig;
